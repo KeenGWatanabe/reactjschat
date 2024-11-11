c9eed0b6501847f66a042d7be4c81acc34e8c54d
KeenGWatanabe <mckeenweb@gmail.com>
2024-11-11 08:55:16 +0800

    r

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
 mongoose.connect(process.env.MONGO_URL);
 //const mongoUrl = 'mongodb+srv://mbs:<password>.yyt41.mongodb.net/?retryWrites=true&w=majority&appName=Chat'
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.get('/test', (req,res)=>{
   res.json('test ok');
+  console.log(process.env.MONGO_URL);
 });
 
 app.post('/register', (req, res) =>{
